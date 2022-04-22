ca380efbe2a4e21bf28a84c37b66ca0fc2ca1175
김용원 <62327209+dydvn@users.noreply.github.com>
2022-04-22 17:32:51 +0900

    Create BackgroundRepeat.cs

---
+++ BackgroundRepeat.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgrounRepeat : MonoBehaviour
+{
+    public Player player;
+    public GameManager_Play gameManager_Play;
+    [Header("[레이어별로 빼주는 속도]")]
+    public float speed;
+
+    [Header("[높이]")]
+    public float height;
+
+    public GameObject testobject;
+
+    private float wall_Size;
+
+    private void Start()
+    {
+        //if (gameManager_Play.nTheme_Num != 5)
+            wall_Size = GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        //gameManager_Play = GameObject.Find("GameManager").GetComponent<GameManager_Play>();
+    }
+
+    private void Update()
+    {
+        transform.Translate((gameManager_Play.move_Speed_Ground + new Vector3(speed * Time.deltaTime, 0, 0)) * player.nRun_Stop * gameManager_Play.nPause);
+
+        if (transform.position.x < -(wall_Size * 4.0f))
+            transform.position = new Vector3(testobject.transform.position.x + wall_Size * 3.95f, height, -0.1f);
+    }
+}
